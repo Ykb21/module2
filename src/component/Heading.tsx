d4ae8888ec7f6b22629bc4d3f64d5d2d29426b3b
student-01 <yakubbuchari12@gmail.com>
2024-06-24 13:33:09 +0700

    ykb

---
+++ src/component/Heading.tsx
@@ -0,0 +1,18 @@
+import { Text } from "@chakra-ui/react";
+
+const Heading = () => {
+  return (
+    <>
+    <Text
+    mb={20}
+    fontWeight="bold"
+    textColor="white"
+    textAlign="center"
+    fontSize="x-large"
+    >
+      TodoList
+    </Text>
+      </>
+  );
+};
+export default Heading
